839ca8001cbd39634c1377e09a67b773173d37e2
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-21 16:43:00 +0900

    comment: "패킷 헤더 관련 상수에 주석으로 설명 추가"

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const TOTAL_LENGTH_SIZE = 4;
-export const HANDLER_ID_SIZE = 2;
+export const TOTAL_LENGTH_SIZE = 4; // 패킷 헤더의 길이 부분 크기 (4바이트)
+export const HANDLER_ID_SIZE = 2; // 패킷 헤더의 핸들러ID 부분 크기 (2바이트)
